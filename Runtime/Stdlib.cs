930452128f9ee8b2a9692d18eb4e93c9ab070b51
OneManMonkeySquad <39922492+OneManMonkeySquad@users.noreply.github.com>
2025-04-27 20:11:19 +0200

    panic

--- Runtime/Stdlib.cs
+++ Runtime/Stdlib.cs
@@ -1,11 +1,23 @@
 using GameKit.Scripting.Internal;
 using Unity.Entities;
 using Unity.Mathematics;
+using UnityEngine;
 
 namespace GameKit.Scripting.Runtime
 {
     public static class Stdlib
     {
+        [Scriptable("panic")]
+        public static void Panic(Value str)
+        {
+            Debug.LogError("PANIC: " + Buildin.GetString(str));
+#if UNITY_EDITOR
+            Debug.Break();
+#else
+            Application.Quit();
+#endif
+        }
+
         [Scriptable("sin")]
         public static Value Sin(Value val) => Value.FromDouble(math.sin((double)val));
 
